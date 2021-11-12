d717e1250f25f9e96f8898276ef09f6c2e62a4fd
CHOCOLVT <pkepfl@outlook.com>
2021-11-12 14:07:08 +0100

    notebook - before final M2

--- notebook/utils.py
+++ notebook/utils.py
@@ -1,7 +1,25 @@
+from datetime import datetime
+
 def get_unique_list(serie):
-    out = []
-    for i in serie.values:
+    '''
+    find unique element, and corresponding index, of a Serie with values of type List()
+    
+    input  : [serie]  : Serie with list for value (might be empty list
+    output : [unique] : List of unique element in the Serie serie
+             [idx]    : index of 1st row of serie containing the corresponding [unique] value
+    '''
+    unique = []
+    idx = []
+    for iiter, i in enumerate(serie.values):
         for j in i:
-            if not j in out:
-                out.append(j)
-    return out
+            if not (j in unique):
+                unique.append(j)
+                idx.append(iiter)
+    return unique, idx
+
+
+def get_week(dataframe, col):
+    return dataframe[col].apply(lambda x: datetime.fromisoformat(x[:-7]).isocalendar()[1])
+
+def get_month(dataframe, col):
+    return dataframe[col].apply(lambda x: int(x[5:7]))
